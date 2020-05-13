f7ba8554cddb3a98666f69080312ebb396ce3c40
Kaviya Ayyappan <kaviya.eie@gmail.com>
2020-05-13 13:26:53 +0200

    Reverse Array

---
+++ ReverseArrayExercise/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseArrayExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[5];
+            Console.WriteLine("Enter the number:");
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Reverse Array");
+            int[] temp = array;
+            Array.Reverse(temp);
+            foreach (int j in temp)
+            {
+                Console.WriteLine(j);
+            }
+            Console.ReadLine();
+        }
+    }
+}
